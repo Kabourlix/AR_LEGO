bcc76d68f897d70d332fad83ae3ee4b69f841a37
alucas2 <>
2022-01-20 16:35:24 +0100

    lego controller

---
+++ Assets/Scripts/Data/LegoController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Data;
+
+public class LegoController : MonoBehaviour
+{
+    public event Action OnNoticeFinished;
+    public event Action OnActionImpossible;
+
+    [SerializeField]
+    private List<GameObject> brickPrefabs;
+    private GameObject currentBrick;
+
+    public void PutBrick(int x, int y, int z, int orientation, string brick, string parent_brick)
+    {
+        /* crée le prefab "parent_brick" si inexistant.
+         * ajoute le prefab "brick" à "parent_brick".
+         */
+    }
+
+
+
+
+
+
+
+
+
+
+    private void CleanCurrentStep()
+    {
+        Destroy(currentBrick);
+    }
+
+    private void StartStep(int x, int y, int z, int orientation, string piece)
+    {
+        Vector3 world_position = new Vector3(
+            x * 5 * 1.6e-3f, // scale to real lego dimensions
+            y * 2 * 1.6e-3f,
+            z * 5 * 1.6e-3f
+        );
+        Quaternion world_orientation = Quaternion.AngleAxis(90 * orientation, Vector3.up);
+        currentBrick = Instantiate(brickPrefabs[0], world_position, world_orientation);
+    }
+}
